9ca771eb839d4236c05c1184c7f4e87937cba854
LENV <technicalpriyanshutaru@gmail.com>
2023-05-31 16:48:36 +0530

    about us is done.

--- lib/screens/about_us/about_us.dart
+++ lib/screens/about_us/about_us.dart
@@ -23,7 +23,7 @@ class AboutUs extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.all(12.0),
         child: Text(
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          "Weddify is a wedding planner app designed to assist couples in planning their wedding. It provides a wide range of features & help in manges budget, vendores & time, this wedding planner app can simplify the planning process , save time and reduce stress for couples preparing for their wedding.\n It's great solution for the big day.",
           style: GoogleFonts.kurale(
             fontSize: 15,
           ),
